660b51f2f05de6a85a9507bf412391502035d348
Kobinarth Panchalingam <kobinarth22@gmail.com>
2024-09-06 12:26:15 +0530

    add counter class with java lock

---
+++ Locks/Counter.java
@@ -0,0 +1,18 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Counter {
+    private int count = 0;
+    private Lock lock = new ReentrantLock();
+
+    public int getAndIncrement() {
+        lock.lock();
+        try {
+            int temp = count;
+            count = temp + 1;
+            return temp;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
---
+++ Locks/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+
+        // No of threads
+        int noOfThreads = 10;
+
+        // Create an array to hold threads
+        Thread[] threads = new Thread[noOfThreads];
+
+        for (int i = 0; i < noOfThreads; i++) {
+            threads[i] = new Thread(
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            for (int j = 0; j < 100; j++) {
+                                System.out.println(Thread.currentThread().getName() + ": " + counter.getAndIncrement());
+                            }
+                        }
+                    }
+            );
+        }
+
+        // Start all thread
+        for (int i = 0; i < noOfThreads; i++) {
+            threads[i].start();
+        }
+
+        // Join all threads
+        for (int i = 0; i < noOfThreads; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        // Print final value
+        System.out.println("Final counter value: "+ counter.getAndIncrement());
+    }
+}
